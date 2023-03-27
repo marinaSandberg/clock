6cc1e25e97c443075773c870a560f0256507c760
marinaSandberg <marina_p13@hotmail.com>
2023-03-27 18:03:36 +0200

    moved folder

---
+++ clock.js
@@ -0,0 +1,26 @@
+(function DateClock() {
+    let dt = document.querySelector("#date");
+    let cl = document.querySelector("#clock");
+    let d = new Date();
+
+    let year = d.getFullYear();
+    let month = d.getMonth();
+    let day = d.getDay();
+    let hours = d.getHours();
+    let min = d.getMinutes();
+    let sec = d.getSeconds();
+
+    if (month < 10) month = "0" + month;
+    if (day < 10) day = "0" + day;
+    if (hours < 10) hours = "0" + hours;
+    if (min < 10) min = "0" + min;
+    if (sec < 10) sec = "0" + sec;
+
+
+    dt.textContent = year + "." + month + "." + day;
+
+    cl.textContent = hours + ":" + min + ":" + sec;
+
+    setTimeout(DateClock, 1000);
+})();
+
